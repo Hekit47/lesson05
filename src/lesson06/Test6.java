2572b998e673c66a1aec46faf8addb8208ec9175
Hekit47 <hekit@mail.ru>
2021-01-20 21:25:50 +0300

    Домашнее задание по уроку №7

--- src/lesson06/Test6.java
+++ src/lesson06/Test6.java
@@ -2,11 +2,13 @@ package lesson06;
 
 //import java.util.Arrays;
 
+import java.util.Random;
+
 public class Test6 {
 
     public static void main(String[] args) {
         // Создаем профиль животного
-         Cat cat1 = new Cat("Barsik",0,0,0);
+         Cat cat1 = new Cat("Barsik",2,0,0); //задаем максимальные значения
          Dog dog1 = new Dog("Jack",0,0,0);
          Dog dog2 = new Dog("Muhtar",0,0,0);
          Cat cat2 = new Cat ("Kruasan", 0,0,0);
@@ -23,4 +25,5 @@ public class Test6 {
         dog2.tryToJump(0.4);
         cat2.tryToJump(1.8);
     }
+
 }
